33ae39d9c289ca9abfd58d726c87e3e29c36fb39
MAtea01 <mateamilutin01@gmail.com>
2023-06-05 18:46:20 +0200

    addUser

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AddUser() {
   let navigate = useNavigate();
@@ -76,9 +76,9 @@ export default function AddUser() {
             <button type="submit" className="btn btn-outline-primary">
               Submit
             </button>
-            <button type="submit" className="btn btn-outline-danger mx-2">
+            <Link className="btn btn-outline-danger mx-2" to="/">
               Cancel
-            </button>
+            </Link>
           </form>
         </div>
       </div>
