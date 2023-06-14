45c6f45c3ee441ff1f3273d58acf1293ea097093
MAtea01 <mateamilutin01@gmail.com>
2023-06-14 10:13:19 +0200

    dodana funkcionlanost na view botun

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -7,9 +7,9 @@ export default function NavBar() {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to="/">
             FullStackApp
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
